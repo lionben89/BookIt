f06e0c267d2d026685c919c3aa30181712f1812a
Dror Brunman <drorbrun.man@gmail.com>
2018-04-19 18:00:17 +0300

    changing settings style to material design

--- BookItSrc/src/app/main/settings/settings.component.ts
+++ BookItSrc/src/app/main/settings/settings.component.ts
@@ -15,3 +15,37 @@ export class SettingsComponent implements OnInit {
   }
   
 }
+
+/**
+ * @title Configurable checkbox
+ */
+@Component({
+  selector: 'checkbox-configurable-example',
+  templateUrl: 'checkbox-configurable-example.html',
+  styleUrls: ['checkbox-configurable-example.css'],
+})
+export class CheckboxConfigurableExample {
+  checked = false;
+  indeterminate = false;
+  align = 'start';
+  disabled = false;
+}
+
+/**
+ * @title Stacked chips
+ */
+@Component({
+  selector: 'chips-stacked-example',
+  templateUrl: 'chips-stacked-example.html',
+  styleUrls: ['chips-stacked-example.css'],
+})
+export class ChipsStackedExample {
+  color: string;
+
+  availableColors = [
+    { name: 'none', color: '' },
+    { name: 'Primary', color: 'primary' },
+    { name: 'Accent', color: 'accent' },
+    { name: 'Warn', color: 'warn' }
+  ];
+}
