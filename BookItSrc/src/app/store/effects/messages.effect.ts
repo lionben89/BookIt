c3cc60e7fa1abf35c0ac6e55dc1508aadbce94dd
Yuval Ben Arie <yuval1010@gmail.com>
2018-06-10 00:00:48 +0300

    Chat: delete stored messages on init

--- BookItSrc/src/app/store/effects/messages.effect.ts
+++ BookItSrc/src/app/store/effects/messages.effect.ts
@@ -38,8 +38,6 @@ export class MessagesEffects {
     // methods
     setMessage(messageSnap) {
         let message : Message = messageSnap.val();
-        console.log("NewMessage");
-        console.log(message);
         this.store.dispatch(new fromMessagesActions.LoadMessagesSuccess([message]));
     }
 
@@ -48,18 +46,14 @@ export class MessagesEffects {
     InitMessageThread: Observable<Action> = this.actions.ofType(fromMessagesActions.ActionsMessagesConsts.INIT_MESSAGE_THREAD)
         .map((action: fromMessagesActions.InitMessageThread) => action.payload)
         .switchMap((threadId: string) => {
-            console.log("LoadUserMessages");
             if (!this.afAuth.authState) {
                 this.store.dispatch(new fromMessagesActions.LoadMessagesFail());
                 return Observable.of(null);
             }
 
-            console.log("Adding listeners");
             this.dbRef.limitToLast(12).orderByChild('threadId').equalTo(threadId).on('child_added', function(self) {
                 return function(messageSnap) {
                     let message : Message = messageSnap.val();
-                    console.log("NewMessage");
-                    console.log(message);
                     self.store.dispatch(new fromMessagesActions.LoadMessagesSuccess([message]));
                     };
             }(this));
@@ -71,13 +65,11 @@ export class MessagesEffects {
     AddMessage: Observable<Action> = this.actions.ofType(fromMessagesActions.ActionsMessagesConsts.ADD_MESSAGE)
         .map((action: fromMessagesActions.AddMessage) => action.payload)
         .switchMap((message: Message) => {
-            console.log("AddMessage");
             if (!this.afAuth.authState) {
                 this.store.dispatch(new fromMessagesActions.AddMessageFail());
                 return Observable.of(null);
             }
 
-            console.log("Adding message");
             this.dbRef.push(message);
             return Observable.of(null);
         });
